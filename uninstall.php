819c71540773df8e30a2ffe8367c147b215e4ff8
James Doc <jamesdoc@gmail.com>
2014-06-23 11:28:48 +0100

    Tweak to uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -4,3 +4,4 @@ if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
 
 delete_option( 'RestrictCats_options' );
 delete_option( 'RestrictCats_user_options' );
+delete_option( 'restrict_categories_user_access' );
